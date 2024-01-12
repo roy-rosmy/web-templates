d52da885e82dace1695e3ab544399f984879c29e
rosmy roy <rosmy.roy@gmail.com>
2024-01-12 17:21:50 +0000

    header

--- web-templates/src/components/header/header.tsx
+++ web-templates/src/components/header/header.tsx
@@ -2,33 +2,31 @@ import PropTypes from "prop-types";
 import styles from "./header.module.css";
 import { Navbar } from 'react-bootstrap'; // Import Bootstrap components
 import Nav from 'react-bootstrap/Nav';
-
+import Container from 'react-bootstrap/Container';
 interface HeaderProps {}
 
 export const Header: React.FunctionComponent<HeaderProps> = (props) => {
   return(
         <div className={styles.header}>
-          <Navbar>
-            <Navbar.Brand href="#home">Kerala Thattukada</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#menu">Menu</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
+          <Navbar fixed="top" bg="light" expand="lg">
+            <Container fluid>
+              <Navbar.Brand href="#home" className="ml-0">
+                <img
+                    alt=""
+                    src="/img/logo.svg"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                Kerala Thattukada
+              </Navbar.Brand>
+                <Nav>
+                  <Nav.Link href="#about">About</Nav.Link>
+                  <Nav.Link href="#menu">Menu</Nav.Link>
+                  <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+            </Container>  
           </Navbar>
-          <div className="navbar-landing">
-            <div className="landing-bar landing-white landing-padding landing-card">
-            <a href="#home" className="landing-bar-item landing-button">Kerala Thattukada</a>
-            <div className="landing-right landing-hide-small">
-              <a href="#about" className="landing-bar-item landing-button">About</a>
-              <a href="#menu" className="landing-bar-item landing-button">Menu</a>
-              <a href="#contact" className="landing-bar-item landing-button">Contact</a>
-            </div>
-          </div>
-        </div>
       </div>
   );
 };
